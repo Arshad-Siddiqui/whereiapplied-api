06bfae9571b2d04ac9a22817c758592a5b8f4551
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-06-07 23:04:11 +0100

    Create init function

--- main.go
+++ main.go
@@ -9,6 +9,27 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	database.Connect()
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/applications", listApplications)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, World!"))
+	})
+
+	log.Println("Listening on port 8080")
+	http.ListenAndServe(":8080", mux)
+}
+
 func listApplications(w http.ResponseWriter, r *http.Request) {
 	applications := database.GetApplications()
 	w.Header().Set("Content-Type", "application/json")
@@ -20,22 +41,4 @@ func listApplications(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonData)
-}
-
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
-
-	database.Connect()
-	mux := http.NewServeMux()
-	mux.HandleFunc("/applications", listApplications)
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello, World!"))
-	})
-
-	log.Println("Listening on port 8080")
-	http.ListenAndServe(":8080", mux)
 }
