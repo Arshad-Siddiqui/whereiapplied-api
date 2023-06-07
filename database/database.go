a57ccb8ec03f8192b47c4111a7cda49f74df7fef
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-06-07 13:19:41 +0100

    Confirm valid database connection

--- database/database.go
+++ database/database.go
@@ -2,17 +2,28 @@ package database
 
 import (
 	"context"
+	"fmt"
+	"os"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func Connect() {
 	ctx := context.TODO()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
 	if err != nil {
 		panic(err)
 	}
+
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(databases)
+
 	defer func() {
 		if err = client.Disconnect(ctx); err != nil {
 			panic(err)
--- main.go
+++ main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
+	"log"
 	"net/http"
+
+	"github.com/Arshad-Siddiqui/whereiapplied-api/database"
+	"github.com/joho/godotenv"
 )
 
+func listApplications(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	database.Connect()
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
-	})
-
-	mux.HandleFunc("/applications", func(w http.ResponseWriter, r *http.Request) {
-
-	})
+	mux.HandleFunc("/applications", listApplications)
 
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	log.Println("Listening on port 8080")
 	server.ListenAndServe()
 }
