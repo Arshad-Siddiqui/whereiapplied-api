04d1765d6734196d6b25acff8d065ff126a4370c
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-06-16 00:54:38 +0100

    Test #GetApplications for stability

--- database/database_test.go
+++ database/database_test.go
@@ -1,12 +1,24 @@
 package database
 
 import (
-	"os"
 	"testing"
 
 	"github.com/joho/godotenv"
 )
 
+func TestConnect(t *testing.T) {
+	// Load the .env file
+	err := godotenv.Load("../.env.test")
+	if err != nil {
+		t.Error("Error loading .env file")
+	}
+
+	// Connect to the database
+	err = Connect()
+	if err != nil {
+		t.Error("Error connecting to the database")
+	}
+}
 func TestAddApplication(t *testing.T) {
 	// Load the .env file
 	err := godotenv.Load("../.env.test")
@@ -14,14 +26,30 @@ func TestAddApplication(t *testing.T) {
 		t.Error("Error loading .env file")
 	}
 
-	t.Log(os.Getenv("MONGO_URI"), "<- MONGO URI")
-
 	// Connect to the database
 	err = Connect()
 	if err != nil {
 		t.Error("Error connecting to the database")
 	}
 	err = AddApplication("Google", "https://google.com")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestGetApplications(t *testing.T) {
+	// Load the .env file
+	err := godotenv.Load("../.env.test")
+	if err != nil {
+		t.Error("Error loading .env file")
+	}
+
+	// Connect to the database
+	err = Connect()
+	if err != nil {
+		t.Error("Error connecting to the database")
+	}
+	_, err = GetApplications()
 	if err != nil {
 		t.Error(err)
 	}
