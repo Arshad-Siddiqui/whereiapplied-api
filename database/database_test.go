c2e38498046a2eb96ff07556198df7cbf138cee9
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-06-15 23:45:29 +0100

    Update gitignore

--- database/database_test.go
+++ database/database_test.go
@@ -1,14 +1,25 @@
 package database
 
 import (
+	"os"
 	"testing"
+
+	"github.com/joho/godotenv"
 )
 
 func TestAddApplication(t *testing.T) {
-	// Connect to the database
-	err := Connect()
+	// Load the .env file
+	err := godotenv.Load("./.env.test")
 	if err != nil {
-		t.Error(err)
+		t.Error("Error loading .env file")
+	}
+
+	t.Log("MONGO_URI: ", os.Getenv("MONGO_URI:"))
+
+	// Connect to the database
+	err = Connect()
+	if err != nil {
+		t.Error("Error connecting to the database")
 	}
 	err = AddApplication("Google", "https://google.com")
 	if err != nil {
